405b8c697efe9f37bb4440ca0ba6529acb354f43
lrabinowicz <lu.rabinowicz@gmail.com>
2017-11-12 15:07:00 -0300

    Ya hace el pdf y lo hashea

--- ServicioAPP/app.py
+++ ServicioAPP/app.py
@@ -10,18 +10,29 @@ def main():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+	# Agarro los datos que completo el usuario
 	first_name = request.form['nombre']
 	webSite = request.form['webSite']
 
-	#pdfkit.from_url(webSite, 'out.pdf')
-	#return hashlib.sha256('EL pdf').hexdigest()
+	# Genero el PDF de la pagina y lo hasheo 
+	pdf = pdfkit.from_url(webSite, False)
+	hashPdf = hashlib.sha256(pdf).hexdigest()
+	
 
-	#Hay que mandar el hash a la API rest de la tsa
+	#Mandar el hash a la API rest de la tsa:
+	
+	## Set up the parameters we want to pass to the API.
+	#parameters = {"hash": hashPdf}
+	## Make a get request with the parameters.
+	#response = requests.get("URL de la API", params=parameters)
+	## Print the content of the response (the data the server returned)
+	#resultadoAFirmar = response.content
+
 	#Firmar lo que devuelve la tsa con paddes
-	# Hacer que se pueda descargar
+	#Hacer que se pueda descargar
 
 	#descarga(pdf) o error(pdf)
-	return 'submit %s %s <br/> <a href="/">Back Home</a>' % (first_name, webSite)
+	#return 'submit %s %s <br/> <a href="/">Back Home</a>' % (first_name, webSite)
 
 #@app.route('/descarga')
 #def descarga():
